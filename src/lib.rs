4a23edbf7d80b90c50f7153ccde3821aad473613
Bart Massey <bart.massey@gmail.com>
2025-06-03 13:36:48 -0700

    fleshed out first version of API

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,65 @@
-use embedded_hal::{digital::{OutputPin, PushPull}, timer};
-use nrf52833_hal::Pwm;
+#![allow(unused)]
 
-pub struct GameAudio<T> {
-    timer: T,
-    pwm: Pwm,
-    speaker: Pin<Output<PushPull>>,
+/*!
+## Game audio library crate for the BBC micro:bit v2 (MB2)
+
+This crate currently provides basic support for starting and
+stopping a background song in a MB2 program.
+*/
+
+use embedded_hal::digital::OutputPin;
+use nrf52833_hal::{pwm, timer};
+
+/// A note in the song.
+pub struct Note {
+    /// Frequency of note in Hz.
+    frequency: u16,
+    /// Duration of note in ms.
+    duration: u16,
 }
 
-pub struct Note {
-    frequency: u16,
-    duration: u16,
-}    
+pub struct Song<'a> {
+    notes: &'a [Note],
+    position: usize,
+}
 
-impl<T: timer::Instance> GameAudio<T> {
-    pub fn new(timer: T, pwm: Pwm, speaker: OutputPin<PushPull>) -> Self {
+impl<'a> Song<'a> {
+    pub fn new(notes: &'a [Note]) -> Self {
+        Self { notes, position: 0 }
+    }
+
+    pub fn restart(&mut self) {
+        self.position = 0;
+    }
+}
+
+pub struct GameAudio<'a, T, P, S> {
+    timer: T,
+    pwm: P,
+    speaker: S,
+    song: Option<Song<'a>>,
+}
+
+impl<'a, T, P, S> GameAudio<'a, T, pwm::Pwm<P>, S>
+where
+    T: timer::Instance,
+    P: pwm::Instance,
+    S: OutputPin,
+{
+    pub fn new(timer: T, pwm: pwm::Pwm<P>, speaker: S) -> Self {
         Self {
             timer,
             pwm,
             speaker,
+            song: None,
         }
     }
 
-    pub fn play(&mut self, song: &[Note]) {
-        todo!()
+    pub fn play(&mut self, song: Song<'a>) {
+        self.song = Some(song);
+    }
+
+    pub fn stop(&mut self) {
+        self.song = None;
     }
 }
